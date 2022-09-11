2e2bbd2f1cc6d036b37d1966288af14d048ffac7
Lilkizarmie <jefferyadolor18@gmail.com>
2022-09-11 14:21:30 +0100

    amount fixation

---
+++ packages/react-app/src/components/AmountIn.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState, useRef } from "react";
+
+import { chevronDown } from "../assets";
+import styles from "../styles";
+import { useOnClickOutside } from "../utils";
+
+const AmountIn = () => {
+  return (
+    <div >
+
+    </div>
+  )
+}
+
+export default AmountIn
---
+++ packages/react-app/src/components/AmountOut.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useRef } from "react";
+
+const AmountOut = () => {
+  return (
+    <div>AmountOut</div>
+  )
+}
+
+export default AmountOut
